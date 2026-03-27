ccd248af0c36901e51276482f66e55813b32d6f1
como update <comoupdate@gmail.com>
2026-03-27 17:50:40 +0000

    I JUST ENABLED FIRESTORE FOR MY FIREBASE CONSOLE
    WHAT DO I NEED TO DO N

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,10 +6,10 @@
  * These values are public and safe to include in the client-side code.
  */
 export const firebaseConfig = {
-  apiKey: "PLACEHOLDER",
+  apiKey: "AIzaSyChQDo3TVeaGr_IkuuONQUW5oR_zH8RSX4",
   authDomain: "studio-756554892-b5591.firebaseapp.com",
   projectId: "studio-756554892-b5591",
   storageBucket: "studio-756554892-b5591.firebasestorage.app",
-  messagingSenderId: "PLACEHOLDER",
-  appId: "PLACEHOLDER"
+  messagingSenderId: "133815451520",
+  appId: "1:133815451520:web:02aea2b1999428a82b0b65"
 };
